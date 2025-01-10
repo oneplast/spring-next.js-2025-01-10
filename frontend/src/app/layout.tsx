ded148d7f530204a1b5a67a58c23fdfb46a087df
Gyeonghun Min <riverpower6@naver.com>
2025-01-10 17:03:55 +0900

    14: add: post/list

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
           <div className="flex gap-2">
             <Link href="/">홈</Link>
             <Link href="/about">소개</Link>
+            <Link href="/post/list">글</Link>
           </div>
         </header>
         <main className="flex-grow border-[2px] border-[blue] p-5">
