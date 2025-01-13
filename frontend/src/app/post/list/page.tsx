c2c77ccb159caad2396e17fd5e153de287ba6979
Gyeonghun Min <riverpower6@naver.com>
2025-01-13 09:31:51 +0900

    15: add: print boolean value

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -22,8 +22,8 @@ export default async function Page() {
             <div>authorId : {item.authorId}</div>
             <div>authorName : {item.authorName}</div>
             <div>title : {item.title}</div>
-            <div>published : {item.published}</div>
-            <div>listed : {item.listed}</div>
+            <div>published : {`${item.published}`}</div>
+            <div>listed : {`${item.listed}`}</div>
           </li>
         ))}
       </ul>
